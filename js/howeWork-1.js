04e590110e75fdcbff12306696eec4edaaa4cd45
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-08-24 09:40:47 +0200

    +

---
+++ js/howeWork-1.js
@@ -0,0 +1,10 @@
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Change code below this line
+    for (let i = min; i <= max; i++) {
+        numbers.push(i);       
+    }
+    return numbers;
+  // Change code above this line
+  return numbers;
+}
