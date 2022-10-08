4e007eb9d79c5d1647df1506a8f7e120dcf90ee1
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-10-08 22:53:29 +0200

    +

--- js/13_14-Throttle-Debounce.js
+++ js/13_14-Throttle-Debounce.js
@@ -589,6 +589,22 @@
 
 //todo ============== Ленивая загрузка ============================================
 
+if ('loading' in HTMLImageElement.prototype) { //? Если поодерживается ленивая загрузка - переделаем src
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+  lazyImages.forEach(img => {
+    img.src = img.dataset.src;
+  });
+} else {
+  const script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+  script.integrity = 'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+  script.crossorigin = 'anonymous';
+  script.referrerpolicy = 'no-referrer';
+
+  document.body.appendChild(script); //? повесили скрипт если не поддерживается ленивая загрузка
+}
+
 const lazyImages = document.querySelectorAll('img[data-src]');
 
 lazyImages.forEach(image => {
@@ -600,3 +616,4 @@ function onImageLoaded(evt) {
   console.log('Картинка загрузилась');
   evt.target.classList.add('appear'); //* повесит класс с появлением при загрузке
 }
+
